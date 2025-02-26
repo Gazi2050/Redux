5f23294336b8cd937caaa941661c122bbe83b6eb
Gazi2050 <gazi45344@gmail.com>
2025-02-26 18:53:01 +0600

    set-up project with react,tailwind,redux

---
+++ redux-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center text-5xl font-bold mt-20">Redux App</h1>
+        </div>
+    );
+};
+
+export default Home;
