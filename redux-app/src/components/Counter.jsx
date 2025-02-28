d751ca524d6e0697f6ce00666b46c2f82d268226
Gazi2050 <gazi45344@gmail.com>
2025-02-28 15:50:09 +0600

    implement basic redux

--- redux-app/src/components/Counter.jsx
+++ redux-app/src/components/Counter.jsx
@@ -1,8 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/CounterSlice";
 
 const Counter = () => {
+    const { count } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center mt-10">
+            <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-md w-72 border border-gray-200">
+                <h2 className="text-2xl font-bold mb-3 text-gray-700">Counter</h2>
+                <p className="text-4xl font-semibold mb-5 text-gray-900">{count}</p>
+                <div className="flex gap-3">
+                    <button
+                        className="px-5 py-2 bg-green-500 text-white rounded-lg shadow-sm hover:bg-green-600 transition"
+                        onClick={() => dispatch(increment())}
+                    >
+                        +
+                    </button>
+                    <button
+                        className="px-5 py-2 bg-red-500 text-white rounded-lg shadow-sm hover:bg-red-600 transition"
+                        onClick={() => dispatch(decrement())}
+                    >
+                        -
+                    </button>
 
+                </div>
+            </div>
         </div>
     );
 };
--- redux-app/src/pages/Home.jsx
+++ redux-app/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Counter from "../components/Counter";
 
 const Home = () => {
     return (
         <div>
             <h1 className="text-center text-5xl font-bold mt-20">Redux App</h1>
+            <Counter />
         </div>
     );
 };
